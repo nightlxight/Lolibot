231be6e13d7208f4910adea31b4534396e284e59
nightlxight <itsbeadj@gmail.com>
2021-09-13 16:47:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
