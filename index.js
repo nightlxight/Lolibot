452cce1c335166839009872f8ad880b73618db75
nightlxight <itsbeadj@gmail.com>
2021-09-13 16:42:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ var fs = require('fs');
 const { token } = require('./data/app/token.json');
 const { client_id } = require('./data//app/client_id.json');
 const { guild_id } = require('./data/app/guild_id.json');
+const { Client, Collection, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
