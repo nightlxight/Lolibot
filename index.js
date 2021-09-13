497e1e34f7eecd22a88ac57e3ad1aa905af5cd00
nightlxight <itsbeadj@gmail.com>
2021-09-13 16:36:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 var fs = require('fs');
 const { token } = require('./data/app/token.json');
 const { client_id } = require('./data//app/client_id.json');
+const { guild_id } = require('./data/app/guild_id.json');
